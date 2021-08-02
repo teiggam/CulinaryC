a48843f98a8fe0ccd0edf463e43f25459b6c561e
Andrew Klima <andrewklima7@gmail.com>
2021-08-02 10:53:54 -0500

    Added all group CRUD to controller

--- CulinaryC/CulinaryC/Controllers/CulinaryController.cs
+++ CulinaryC/CulinaryC/Controllers/CulinaryController.cs
@@ -14,7 +14,35 @@ namespace CulinaryC.Controllers
         //Create a Database object
         CookBookContext db = new CookBookContext();
 
-        //-------------- GET ALL USERS ----------------------
+        
+
+        //-------------- LIST ALL Recipes --------------------
+        [HttpGet("Recipe")]
+        public List<Recipes> GetRecipes()
+        {
+            List<Recipes> recipeList = db.Recipes.ToList();
+            return recipeList;
+        }
+
+        //------------- USERs ------------------
+        //When you register a new user, grab the email from Identity and add it to a users object
+        [HttpPost("GetEmail/e={email}")]
+        public void AddUser(string email)
+        {
+            AspNetUsers a = db.AspNetUsers.Where(x => x.Email == email).ToList().First();
+
+            Users u = new Users();
+
+            u.LoginId = a.Email;
+
+            u.Name = "Null";
+            u.Score = 0;
+
+            db.Users.Add(u);
+            db.SaveChanges();
+        }
+
+        //get all users
         [HttpGet("Leaderboard")]
         public List<Users> GetUsers()
         {
@@ -27,35 +55,25 @@ namespace CulinaryC.Controllers
             return usersList;
         }
 
-        //-------------- LIST ALL Recipes --------------------
-        [HttpGet("Recipe")]
-        public List<Recipes> GetRecipes()
+        [HttpGet("UserId={id}")]
+        public Users GetUsersById(int id)
         {
-            List<Recipes> recipeList = db.Recipes.ToList();
-            return recipeList;
+            Users u = db.Users.Where(x => x.Id == id).ToList().First();
+            return u;
         }
 
-        //------------- ADD NEW USER ------------------
-        //When you register a new user, grab the email from Identity and add it to a users object
-        [HttpPost("GetEmail/e={email}")]
-        public void AddUser(string email)
+        [HttpGet("Login={email}")]
+        public Users GetUsersById(string email)
         {
-            AspNetUsers a = db.AspNetUsers.Where(x => x.Email == email).ToList().First();
-
-            Users u = new Users();
-            
-            u.LoginId = a.Email;
-
-            u.Name = "Null";
-            u.Score = 0;
-
-            db.Users.Add(u);
-            db.SaveChanges();
+            Users u = db.Users.Where(x => x.LoginId.ToLower() == email.ToLower()).ToList().First();
+            return u;
         }
 
+        //-----------------Group-------------------------------
         //display group to a user
+        //displaying own groups
         [HttpGet("GetGroup/userId={id}")]
-        public List<Group> GetGroups(int id)
+        public List<Group> GetGroupsByUser(int id)
         {
 
             List<Group> groups = new List<Group>();
@@ -65,15 +83,35 @@ namespace CulinaryC.Controllers
             return groups;
         }
 
+        [HttpGet("GetGroup/GroupName={name}")]
+        public List<Group> GetGroups(string name)
+        {
+
+            List<Group> groups = new List<Group>();
+
+            groups = db.Group.Where(x => x.GroupName.ToLower() == name.ToLower()).ToList();
+
+            return groups;
+        }
+
+        [HttpGet("GetGroup/Id={id}")]
+        public Group GetGroupById(int id)
+        {
+            Group g = db.Group.Where(x => x.UserId == id).ToList().First();
+
+            return g;
+        }
+
         //--------------- Need to test This!! ------------------
         //Add user to a group
+        //on accepting an invitation
         [HttpPost("AddUserToGroup/e={email}&gn={groupName}")]
         public void AddUserToGroup(string email, string groupName)
         {
             //create a new group object
             Group g = new Group();
             //find a user by the email 
-            Users u = db.Users.Where(x => x.LoginId == email).ToList().First();
+            Users u = db.Users.Where(x => x.LoginId.ToLower() == email.ToLower()).ToList().First();
             //get the user id to the group id
             g.UserId = u.Id;
             //update groupName through paremeter
@@ -82,6 +120,48 @@ namespace CulinaryC.Controllers
             db.Group.Add(g);
             db.SaveChanges();
         }
+
+        //creating group
+        //adding group will be on the profile page
+        //inside the group page is where you can find the button to invite users
+        [HttpPost("CreateGroup/gname={name}&userId={id}")]
+        public void CreateGroup(string name, int id)
+        {
+            Group g = new Group();
+            g.GroupName = name;
+            g.UserId = id;
+            List<Group> groups = db.Group.Where(x => x.GroupName == name).ToList();
+            if(groups.Count == 0)
+            {
+                db.Group.Add(g);
+                db.SaveChanges();
+            }
+        }
+
+        //delete user from group
+        [HttpDelete("removeuser={id}&n={groupName}")]
+        public void RemoveUserFromGroup(int id, string groupName)
+        {
+            List<Group> groups = db.Group.Where(x => x.GroupName == groupName).ToList();
+            Group user = groups.Where(x => x.UserId == id).ToList().First();
+
+            db.Group.Remove(user);
+            db.SaveChanges();
+        }
+
+        //only viewable to the user who created the group
+        [HttpDelete("removegroup/gname={name}")]
+        public void RemoveGroup(string name)
+        {
+            List<Group> groups = db.Group.Where(x => x.GroupName == name).ToList();
+            foreach(Group g in groups)
+            {
+                db.Group.Remove(g);
+            }
+            db.SaveChanges();
+        }
+
+        //------------------------Favorites-----------------------------------
 
     }
 }
--- CulinaryC/CulinaryC/Migrations/20210802140121_gettables.cs
+++ CulinaryC/CulinaryC/Migrations/20210802140121_gettables.cs
@@ -2,7 +2,7 @@
 
 namespace CulinaryC.Migrations
 {
-    public partial class Initial : Migration
+    public partial class gettables : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
