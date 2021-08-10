3a2c71b2e7993209d5850314396761893ecb05b7
Kate <k8datema@gmail.com>
2021-08-09 22:05:32 -0400

    fixed it!

--- CulinaryC/CulinaryC/ClientApp/src/Friends.js
+++ CulinaryC/CulinaryC/ClientApp/src/Friends.js
@@ -1,11 +1,3 @@
 "use strict";
-// To parse this data:
-//
-//   import { Convert } from "./file";
-//
-//   const friends = Convert.toFriends(json);
-//
-// These functions will throw an error if the JSON doesn't
-// match the expected interface, even if the JSON is valid.
 Object.defineProperty(exports, "__esModule", { value: true });
 //# sourceMappingURL=Friends.js.map
